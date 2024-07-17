ecb056b801fa4745ab87123bfb0a372df7fc50f6
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-17 13:29:13 +0200

    Added docker

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { db } from "./drizzle/db";
 import { userTable } from "./drizzle/schema";
 
 async function main() {
-  /* await db.insert(userTable).values({
+  await db.insert(userTable).values({
     name: "Jonathan",
   });
   const user = await db.query.userTable.findFirst();
-  console.log(user); */
+  console.log(user);
 }
 
 main();
