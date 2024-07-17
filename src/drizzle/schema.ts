77c766e38e5ea9ba6e4a7d8a4eb54c7d8a6c087a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-17 10:55:13 +0200

    added index for email

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,11 +1,26 @@
-import { pgTable, uuid, varchar, integer, pgEnum } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  varchar,
+  integer,
+  pgEnum,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
 
 export const UserRole = pgEnum("userRole", ["admin", "basic"]);
 
-export const userTable = pgTable("user_table", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 255 }).notNull(),
-  age: integer("age").notNull(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  role: UserRole("userRole").default("basic").notNull(),
-});
+export const userTable = pgTable(
+  "user_table",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    age: integer("age").notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    role: UserRole("userRole").default("basic").notNull(),
+  },
+  (table) => {
+    return {
+      emailIndex: uniqueIndex("emailIndex").on(table.email),
+    };
+  }
+);
